e164587770fd9029ad022122315974cc1da35866
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-07-13 13:08:14 +0100

    completed DB schema for tours and travels

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Travel extends Model
 {
-    use HasFactory, Sluggable;
+    use HasFactory, Sluggable, HasUuids;
 
     protected $table = 'travels';
 
@@ -20,6 +23,11 @@ class Travel extends Model
         'number_of_days',
     ];
 
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
     public function sluggable(): array 
     {
         return [
@@ -27,5 +35,15 @@ class Travel extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+
+    // an accessor
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => $attributes['number_of_days'] - 1
+        );
     }
 }
