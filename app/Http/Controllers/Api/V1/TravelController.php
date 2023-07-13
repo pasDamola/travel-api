9a4123625224fe9a86b5945f730c05c1eb996783
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-07-13 14:04:53 +0100

    first task done, filtered by is_public and pagination

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Travel;
+use App\Http\Resources\TravelResource;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,7 @@ class TravelController extends Controller
     //
     public function index()
     {
-        return Travel::where('is_public', true)->get();
+        $travels = Travel::where('is_public', true)->paginate();
+        return TravelResource::collection($travels);
     }
 }
