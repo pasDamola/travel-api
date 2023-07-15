c4bc02c68f3628622fc87390c699a018f8a658a2
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-07-15 22:29:37 +0100

    fleshed out the artisan create command for users and the RoleSeeder

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create new users';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user['name'] = $this-ask('Name of the new user');
+        $user['email'] = $this-ask('Email of the new user');
+        $user['password'] = $this-secret('Password of the new user');
+
+        $roleName = $this-choice('Role of the new user', ['admin', 'editor'], 1);
+
+        $role = Role::where('name', $roleName)->first();
+
+        if(!$role) {
+            $this->error('Role not found');
+
+            return -1;
+        }
+
+        User::create($user);
+
+        $this->info('User '.$user['email'].' created successfullly');
+    }
+}
